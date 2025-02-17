d7a38ce19a8dc4428262c06a5855012d75652628
Atique Ahmed <atique.ahmed@zoom.us>
2025-02-17 13:14:26 +0530

    chagnes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
-const userServices = require('../services/user')
+const userServices = require('../services/user');
+const crypto = require('crypto')
 const config = require('config');
 
 
@@ -17,16 +18,27 @@ exports.getMeetingWebHooks = async (req, res) => {
   try {
     // Extract query parameters from the URL
     const { provider, endpoint_signing_key } = req.query;
+
+    if(req.body.event === 'endpoint.url_validation') {
+      const hashForValidate = crypto.createHmac('sha256', endpoint_signing_key).update(req.body.payload.plainToken).digest('hex');
+      console.log("plainToken:", req.body.payload.plainToken);
+      console.log("encryptedToken:", hashForValidate);
+
+      return res.status(200).send({
+        "plainToken": req.body.payload.plainToken,
+        "encryptedToken": hashForValidate
+      })
+    }
     
     // Validate if the required parameters exist
     if (!provider || !endpoint_signing_key) {
       return res.status(400).send({ message: "Missing provider & endpoint_signing_key !!!" });
     }
 
-    // Assuming userServices.getMeetingConfiguration can work with query parameters
-    const data = await userServices.getMeetingConfiguration({ provider, endpoint_signing_key });
+    // // Assuming userServices.getMeetingConfiguration can work with query parameters
+    // const data = await userServices.getMeetingConfiguration({ provider, endpoint_signing_key });
     
-    res.status(200).send(data);
+    // res.status(200).send(data);
   } catch (err) {
     res.status(400).send({ message: err.message || err });
   }
